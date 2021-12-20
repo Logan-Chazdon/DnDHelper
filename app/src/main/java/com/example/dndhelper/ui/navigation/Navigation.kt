c5e508be7e046b132f28945cc7deb948e3dad387
Logan Chazdon <lchazdon@cnm.edu>
2021-12-19 17:38:59 -0700

    Making newCharacterView a sub navigation mesh.

--- app/src/main/java/com/example/dndhelper/ui/navigation/Navigation.kt
+++ app/src/main/java/com/example/dndhelper/ui/navigation/Navigation.kt
@@ -6,6 +6,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import com.example.dndhelper.ui.character.AllCharactersView
 import com.example.dndhelper.ui.character.AllCharactersViewModel
 import com.example.dndhelper.ui.character.CharacterMainView
@@ -32,39 +33,41 @@ fun Navigation(navController: NavHostController) {
 
 
 
-        composable("newCharacterView/BackgroundView") {
-            BackgroundView()
-        }
-        composable("newCharacterView/ClassView") {
-            val viewModel = hiltViewModel<NewCharacterViewModel>()
-            ClassView(viewModel, navController = navController)
-        }
-        composable("newCharacterView/ClassView/ConfirmClassView/{classIndex}") {backStackEntry ->
-            backStackEntry.arguments?.getString("classIndex")?.toInt()?.let {
-                ConfirmClassView(
-                    viewModel = hiltViewModel<NewCharacterViewModel>(),
-                    classIndex = it
-                )
+        navigation(startDestination = "newCharacterView/ClassView", route = "newCharacterView") {
+            composable("newCharacterView/BackgroundView") {
+                BackgroundView()
             }
-        }
-        composable("newCharacterView/ConfirmBackGroundView") {
-            ConfirmBackgroundView()
-        }
-        composable("newCharacterView/ConfirmRaceView/{raceIndex}") {backStackEntry ->
-            backStackEntry.arguments?.getString("raceIndex")?.toInt()?.let {
-                ConfirmRaceView(
-                    viewModel = hiltViewModel<NewCharacterViewModel>(),
-                    raceIndex = it
-                )
+            composable("newCharacterView/ClassView") {
+                val viewModel = hiltViewModel<NewCharacterViewModel>()
+                ClassView(viewModel, navController = navController)
             }
-        }
-        composable("newCharacterView/RaceView") {
-            val viewModel = hiltViewModel<NewCharacterViewModel>()
-            RaceView(viewModel, navController = navController)
-        }
-        composable("newCharacterView/statsView") {
-            val viewModel = hiltViewModel<NewCharacterViewModel>()
-            StatsView(viewModel)
+            composable("newCharacterView/ClassView/ConfirmClassView/{classIndex}") { backStackEntry ->
+                backStackEntry.arguments?.getString("classIndex")?.toInt()?.let {
+                    ConfirmClassView(
+                        viewModel = hiltViewModel<NewCharacterViewModel>(),
+                        classIndex = it
+                    )
+                }
+            }
+            composable("newCharacterView/ConfirmBackGroundView") {
+                ConfirmBackgroundView()
+            }
+            composable("newCharacterView/ConfirmRaceView/{raceIndex}") { backStackEntry ->
+                backStackEntry.arguments?.getString("raceIndex")?.toInt()?.let {
+                    ConfirmRaceView(
+                        viewModel = hiltViewModel<NewCharacterViewModel>(),
+                        raceIndex = it
+                    )
+                }
+            }
+            composable("newCharacterView/RaceView") {
+                val viewModel = hiltViewModel<NewCharacterViewModel>()
+                RaceView(viewModel, navController = navController)
+            }
+            composable("newCharacterView/statsView") {
+                val viewModel = hiltViewModel<NewCharacterViewModel>()
+                StatsView(viewModel)
+            }
         }
     }
 }

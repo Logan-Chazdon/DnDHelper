34bb2b02c5a03f9580288e60f82537a8b9785509
Logan Chazdon <lchazdon@cnm.edu>
2021-12-23 19:35:34 -0700

    Implementing class Icons.

--- app/src/main/java/com/example/dndhelper/ui/newCharacter/ClassView.kt
+++ app/src/main/java/com/example/dndhelper/ui/newCharacter/ClassView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,8 +16,11 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.dndhelper.R
+
 import androidx.navigation.NavController
 
 @Composable
@@ -28,6 +32,22 @@ fun ClassView(
     val classes by viewModel.classes.observeAsState()
     val scrollState = rememberScrollState()
     viewModel.id = characterId
+    val classIcons = listOf(
+        painterResource(R.drawable.ic_class_icon___artificer),
+        painterResource(R.drawable.ic_class_icon___barbarian),
+        painterResource(R.drawable.ic_class_icon___bard),
+        painterResource(R.drawable.ic_class_icon___cleric),
+        painterResource(R.drawable.ic_class_icon___druid),
+        painterResource(R.drawable.ic_class_icon___fighter),
+        painterResource(R.drawable.ic_class_icon___monk),
+        painterResource(R.drawable.ic_class_icon___paladin),
+        painterResource(R.drawable.ic_class_icon___ranger),
+        painterResource(R.drawable.ic_class_icon___rogue),
+        painterResource(R.drawable.ic_class_icon___sorcerer),
+        painterResource(R.drawable.ic_class_icon___warlock),
+        painterResource(R.drawable.ic_class_icon___wizard)
+    )
+
     Column(
         modifier = Modifier
             .fillMaxHeight(0.95f)
@@ -48,10 +68,17 @@ fun ClassView(
                     }
             )
             {
-              Text(
-                  item.name,
-                  fontSize = 24.sp,
-                )
+              Row()
+              {
+                  Icon(
+                      painter = classIcons[i],
+                      contentDescription = "${item.name} Icon"
+                  )
+                  Text(
+                      item.name,
+                      fontSize = 24.sp,
+                  )
+              }
             }
             if(i != classes!!.size - 1)
             {

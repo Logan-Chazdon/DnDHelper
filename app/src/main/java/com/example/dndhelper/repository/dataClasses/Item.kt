7353426b7e40d9b64db6865a16f97c1c87167228
Logan Chazdon <lchazdon@cnm.edu>
2021-12-31 04:39:56 -0700

    Updating fields.

--- app/src/main/java/com/example/dndhelper/repository/dataClasses/Item.kt
+++ app/src/main/java/com/example/dndhelper/repository/dataClasses/Item.kt
@@ -2,5 +2,8 @@ package com.example.dndhelper.repository.dataClasses
 
 open class Item (
     val name : String? = null,
-    val index: String? = null
+    val index: String? = null,
+    val desc: String? = null,
+    val itemRarity : String? = null,
+    val cost : List<Currency>? = null
 )

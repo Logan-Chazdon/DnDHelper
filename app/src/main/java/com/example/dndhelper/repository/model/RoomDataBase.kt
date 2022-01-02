9de068aefb6e43e9401d5e677856b0b319cba6c9
Logan Chazdon <lchazdon@cnm.edu>
2022-01-02 11:24:57 -0700

    Incrementing version.

--- app/src/main/java/com/example/dndhelper/repository/model/RoomDataBase.kt
+++ app/src/main/java/com/example/dndhelper/repository/model/RoomDataBase.kt
@@ -10,7 +10,7 @@ import com.example.dndhelper.repository.dataClasses.Character
 import com.example.dndhelper.repository.dataClasses.Race
 
 
-@Database(entities = [(Character::class), (Class::class), (Race::class)], version = 20)
+@Database(entities = [(Character::class), (Class::class), (Race::class)], version = 21)
 @TypeConverters(Converters::class)
 abstract class RoomDataBase: RoomDatabase() {
     abstract fun databaseDao(): DatabaseDao

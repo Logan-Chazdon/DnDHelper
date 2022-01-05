92810ade06255e19e4b37fc860e1dc31edf598c6
Logan Chazdon <lchazdon@cnm.edu>
2022-01-05 08:38:33 -0700

    Changing ec to ep.

--- app/src/main/java/com/example/dndhelper/repository/dataClasses/Currency.kt
+++ app/src/main/java/com/example/dndhelper/repository/dataClasses/Currency.kt
@@ -13,7 +13,7 @@ data class Currency(
     override val index: String? = null
     override val desc: String? = null
     override val itemRarity: String? = null
-    override val cost: List<Currency>? = null
+    override val cost: Map<String, Currency>? = null
 
     public val abbreviatedName : String
     get() {
@@ -47,30 +47,30 @@ data class Currency(
         private val currencyValues = mapOf(
             "cp" to 1,
             "sp" to 10,
-            "ec" to 50,
+            "ep" to 50,
             "gp" to 100,
             "pp" to 1000
         )
 
-        fun getEmptyCurrencyList(): List<Currency> {
-            return listOf(
-                Currency(
+        fun getEmptyCurrencyMap(): Map<String, Currency> {
+            return mapOf(
+                "pp" to Currency(
                     name = "Platinum pieces",
                     amount = 0
                 ),
-                Currency(
+                "gp" to  Currency(
                     name = "Gold pieces",
                     amount = 0
                 ),
-                Currency(
+                "ep" to Currency(
                     name = "Electrum pieces",
                     amount = 0
                 ),
-                Currency(
+                "sp" to Currency(
                     name = "Silver pieces",
                     amount = 0
                 ),
-                Currency(
+                "cp" to Currency(
                     name = "Copper pieces",
                     amount = 0
                 )

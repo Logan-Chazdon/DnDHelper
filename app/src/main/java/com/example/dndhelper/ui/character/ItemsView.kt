2bb9bccdcf22f61de2a62cd85762f1fc8c7c213f
Admin <loganchazdon>
2022-01-16 03:04:42 -0700

    Upgrading item add dialog to be a dialog and have a search bar.

--- app/src/main/java/com/example/dndhelper/ui/character/ItemsView.kt
+++ app/src/main/java/com/example/dndhelper/ui/character/ItemsView.kt
@@ -10,18 +10,26 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.example.dndhelper.repository.dataClasses.Armor
 import com.example.dndhelper.repository.dataClasses.Weapon
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
+@ExperimentalComposeUiApi
 @ExperimentalFoundationApi
 @Composable
 fun ItemsView(viewModel : ItemViewModel) {
@@ -179,91 +187,127 @@ fun ItemsView(viewModel : ItemViewModel) {
 
 
         if (expanded) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+            Dialog(
+                onDismissRequest = { expanded = false },
+                properties = DialogProperties(
+                    usePlatformDefaultWidth = false,
+                    dismissOnClickOutside = true
+                )
             ) {
-                Card(
-                    modifier = Modifier.fillMaxSize(0.8f),
-                    shape = RoundedCornerShape(10.dp),
-                    elevation = 10.dp
+                Column(
+                    modifier = Modifier.fillMaxSize(0.85f),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    val allItems = viewModel.allItems?.observeAsState()
-                    Column {
-                        var selected by remember { mutableStateOf(-1) }
-                        val scrollState = rememberScrollState()
-                        Column(
-                            modifier = Modifier
-                                .fillMaxHeight(0.8f)
-                                .verticalScroll(scrollState)
-                        ) {
-                            allItems?.value?.forEachIndexed { i, item ->
-                                val color = if (selected == i) {
-                                    MaterialTheme.colors.primary
-                                } else {
-                                    Color.White
-                                }
-                                Card(
-                                    shape = RoundedCornerShape(5.dp),
-                                    elevation = 2.dp,
-                                    modifier = Modifier
-                                        .clickable { selected = i }
-                                        .fillMaxWidth(),
-                                    backgroundColor = color
-                                ) {
-                                    Row(
-                                        Modifier.padding(5.dp)
-                                    ) {
-                                        Text(text = item.name!!)
-                                    }
-                                }
-                            }
-                        }
-
-                        val enabled = selected != -1
-                        Column(
-                            modifier = Modifier.fillMaxSize(),
-                            verticalArrangement = Arrangement.Bottom
-                        ) {
+                    Card(
+                        modifier = Modifier.fillMaxSize(),
+                        shape = RoundedCornerShape(10.dp),
+                        elevation = 10.dp
+                    ) {
+                        val allItems = viewModel.allItems?.observeAsState()
+                        Column {
+                            var selected by remember { mutableStateOf(-1) }
+                            val scrollState = rememberScrollState()
+                            var search by remember { mutableStateOf("") }
                             Row(
-                                horizontalArrangement = Arrangement.End,
-                                modifier = Modifier.fillMaxWidth()
+                                Modifier
+                                    .fillMaxWidth(),
                             ) {
-                                Button(
-                                    enabled = enabled,
-                                    onClick = {
-                                        GlobalScope.launch {
-                                            viewModel.addItem(selected)
-                                            selected = -1
-                                            expanded = false
-                                        }
-                                    }
-                                ) {
-                                    Text("Add Item")
-                                }
-
-                                Spacer(Modifier.width(10.dp))
-
-                                Button(
-                                    enabled = if (enabled) {
-                                        allItems?.value?.get(selected)?.cost != null
-                                    } else {
-                                        false
+                                TextField(
+                                    value = search,
+                                    onValueChange = {
+                                        search = it
                                     },
-                                    onClick = {
-                                        GlobalScope.launch {
-                                            viewModel.buyItem(selected)
-                                            selected = -1
-                                            expanded = false
+                                    singleLine = true,
+                                    textStyle = TextStyle.Default.copy(fontSize = 20.sp),
+                                    modifier = Modifier.fillMaxWidth(),
+                                    trailingIcon = {
+                                        Icon(
+                                            Icons.Default.Search,
+                                            "Search"
+                                        )
+                                    }
+                                )
+                            }
+
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxHeight(0.75f)
+                                    .verticalScroll(scrollState)
+                            ) {
+                                allItems?.value?.forEachIndexed { i, item ->
+                                    //TODO upgrade search
+                                    if (
+                                        search == "" ||
+                                        item.name?.lowercase()?.contains(search.lowercase()) == true
+                                    ) {
+                                        val color = if (selected == i) {
+                                            MaterialTheme.colors.primary
+                                        } else {
+                                            Color.White
+                                        }
+                                        Card(
+                                            shape = RoundedCornerShape(5.dp),
+                                            elevation = 2.dp,
+                                            modifier = Modifier
+                                                .clickable { selected = i }
+                                                .fillMaxWidth(),
+                                            backgroundColor = color
+                                        ) {
+                                            Row(
+                                                Modifier.padding(5.dp)
+                                            ) {
+                                                Text(text = item.name!!)
+                                            }
                                         }
                                     }
-                                ) {
-                                    Text("Buy Item")
                                 }
                             }
-                        }
 
+                            val enabled = selected != -1
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Bottom
+                            ) {
+                                Row(
+                                    horizontalArrangement = Arrangement.End,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Button(
+                                        enabled = enabled,
+                                        onClick = {
+                                            GlobalScope.launch {
+                                                viewModel.addItem(selected)
+                                                selected = -1
+                                                expanded = false
+                                            }
+                                        }
+                                    ) {
+                                        Text("Add Item")
+                                    }
+
+                                    Spacer(Modifier.width(10.dp))
+
+                                    Button(
+                                        enabled = if (enabled) {
+                                            allItems?.value?.get(selected)?.cost != null
+                                        } else {
+                                            false
+                                        },
+                                        onClick = {
+                                            GlobalScope.launch {
+                                                viewModel.buyItem(selected)
+                                                selected = -1
+                                                expanded = false
+                                            }
+                                        }
+                                    ) {
+                                        Text("Buy Item")
+                                    }
+                                }
+                            }
+
+                        }
                     }
                 }
             }

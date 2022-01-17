0e93a0c0c05d4e1e593410214fc770bcfb7c0470
Admin <loganchazdon>
2022-01-17 03:41:36 -0700

    Laying out a prototype for UI.
    Signed-off-by: Admin <loganchazdon>

--- app/src/main/java/com/example/dndhelper/ui/character/CharacterMainView.kt
+++ app/src/main/java/com/example/dndhelper/ui/character/CharacterMainView.kt
@@ -1,13 +1,12 @@
 package com.example.dndhelper.ui.character
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,41 +15,31 @@ import kotlinx.coroutines.launch
 @Composable
 fun CharacterMainView( viewModel: CharacterMainViewModel) {
     val scope = rememberCoroutineScope()
-    Column() {
-        Row() {
-            TextField(
-                value = viewModel.character?.observeAsState()?.value?.name ?: "",
-                onValueChange = {
-                    scope.launch(Dispatchers.IO) {
-                        viewModel.setName(it)
-                    }
-                }
-            )
-
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Card(
+            modifier = Modifier.fillMaxSize(0.97f)
+        ) {
             Column() {
-                Button(
-                    onClick = {
-                        scope.launch(Dispatchers.IO) {
-                            viewModel.longRest()
-                        }
-                    }
-                ) {
-                    Text("Long rest")
-                }
-                Button(
-                    onClick = {
-                        scope.launch(Dispatchers.IO) {
-                            viewModel.shortRest()
-                        }
-                    }
-                ) {
-                    Text("Short rest")
+                Row() {
+                    TextField(
+                        value = viewModel.character?.observeAsState()?.value?.name ?: "",
+                        onValueChange = {
+                            scope.launch(Dispatchers.IO) {
+                                viewModel.setName(it)
+                            }
+                        },
+                        modifier = Modifier.fillMaxWidth(0.6f)
+                    )
+                    FeaturesAndTraitsView(
+                        features = viewModel.characterFeatures.observeAsState(listOf()).value,
+                        modifier = Modifier.fillMaxHeight(0.4f)
+                    )
                 }
             }
         }
-        FeaturesAndTraitsView(
-            features = viewModel.characterFeatures.observeAsState(listOf()).value,
-            modifier = Modifier
-        )
     }
 }

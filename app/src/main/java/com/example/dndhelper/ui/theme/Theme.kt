a17afb2970eb6fb862ba5b1ef76d19dba661cd95
Logan <loganchazdon@gmail.com>
2022-01-26 14:14:38 -0700

    Adding dark mode.
    Signed-off-by: Logan <loganchazdon@gmail.com>

--- app/src/main/java/com/example/dndhelper/ui/theme/Theme.kt
+++ app/src/main/java/com/example/dndhelper/ui/theme/Theme.kt
@@ -1,11 +1,18 @@
 package com.example.dndhelper.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import com.example.dndhelper.dataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -29,9 +36,15 @@ private val LightColorPalette = lightColors(
     */
 )
 
+@SuppressLint("FlowOperatorInvokedInComposition")
 @Composable
 fun DnDHelperTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
+    val overrideDarkModeFlow: Flow<Boolean> = LocalContext.current.dataStore.data
+        .map { preferences ->
+            preferences[booleanPreferencesKey("dark_mode")] ?: false
+        }
+
+    val colors = if(darkTheme || overrideDarkModeFlow.collectAsState(initial = false).value) {
         DarkColorPalette
     } else {
         LightColorPalette

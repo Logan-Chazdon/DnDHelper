082baacd051b2e83d5039acc031ed990e90a1a2a
Logan <loganchazdon@gmail.com>
2022-01-26 18:13:44 -0700

    Updating colors.
    Signed-off-by: Logan <loganchazdon@gmail.com>

--- app/src/main/java/com/example/dndhelper/ui/character/DeathSavesView.kt
+++ app/src/main/java/com/example/dndhelper/ui/character/DeathSavesView.kt
@@ -1,11 +1,8 @@
 package com.example.dndhelper.ui.character
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Checkbox
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,19 +23,15 @@ fun DeathSavesView(
         ) {
             Text(text = type)
             Row(
-                modifier = Modifier.padding(5.dp)
+                modifier = Modifier.padding(2.dp)
             ) {
                 for (i in 0..2) {
                     Checkbox(
-                        modifier = Modifier.background(
-                            shape = CircleShape,
-                            color = MaterialTheme.colors.surface
-                        ),
                         checked = (i < num ?: 0),
                         onCheckedChange = onClick
                     )
                     if (i != 2)
-                        Spacer(modifier = Modifier.width(12.dp))
+                        Spacer(modifier = Modifier.width(6.dp))
                 }
             }
         }

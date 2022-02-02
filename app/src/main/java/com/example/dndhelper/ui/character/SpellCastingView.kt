9470478d44c6d0d54a2b612f222e36f4bd6cca02
Logan Chazdon <loganchazdon@gmail.com>
2022-02-01 19:05:45 -0700

    Adding Icon.
    Signed-off-by: Logan Chazdon <loganchazdon@gmail.com>

--- app/src/main/java/com/example/dndhelper/ui/character/SpellCastingView.kt
+++ app/src/main/java/com/example/dndhelper/ui/character/SpellCastingView.kt
@@ -1,18 +1,24 @@
 package com.example.dndhelper.ui.character
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -36,6 +42,9 @@ fun SpellCastingView(
         elevation = 5.dp,
         modifier = modifier
     ) {
+        var spellLevelsExpanded = remember {
+            mutableStateListOf(true, true, true, true, true, true, true, true, true, true)
+        }
         LazyColumn(
             state = state,
             modifier = Modifier.fillMaxSize(),
@@ -55,7 +64,27 @@ fun SpellCastingView(
                         modifier = Modifier.fillMaxWidth(0.9f).padding(5.dp),
                         horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.Start)
                     ) {
-                        Text(text = slots.name)
+                        Row(
+                            modifier = Modifier.clickable {
+                                spellLevelsExpanded[slotLevel] = !spellLevelsExpanded[slotLevel]
+                            }
+                        ) {
+                            val angle: Float by animateFloatAsState(
+                                targetValue = if (spellLevelsExpanded[slotLevel]) 0F else -90F,
+                                animationSpec = tween(
+                                    durationMillis = 150, // duration
+                                    easing = FastOutSlowInEasing
+                                )
+                            )
+
+                            Text(text = slots.name)
+                            Icon(
+                                Icons.Default.ArrowDropDown, "Drop down",
+                                Modifier.rotate(angle)
+                            )
+
+                        }
+
                         Row(
                             modifier = Modifier.fillMaxWidth(),
                             horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End)
@@ -94,32 +123,34 @@ fun SpellCastingView(
                     }
                 }
 
-                items(spells?.size ?: 0) { i ->
-                    val spell = spells!![i]
-                    Card(
-                        modifier = Modifier.fillMaxWidth(0.95f),
-                        elevation = 2.dp
-                    ) {
-                        Row(
-                            modifier = Modifier.padding(2.dp),
-                            horizontalArrangement = Arrangement.spacedBy(2.dp),
+                if (spellLevelsExpanded[slotLevel]) {
+                    items(spells?.size ?: 0) { i ->
+                        val spell = spells!![i]
+                        Card(
+                            modifier = Modifier.fillMaxWidth(0.95f),
+                            elevation = 2.dp
                         ) {
-                            Text(spell.name, Modifier.width(100.dp))
+                            Row(
+                                modifier = Modifier.padding(2.dp),
+                                horizontalArrangement = Arrangement.spacedBy(2.dp),
+                            ) {
+                                Text(spell.name, Modifier.width(100.dp))
 
-                            Text(spell.damage, Modifier.width(100.dp))
+                                Text(spell.damage, Modifier.width(100.dp))
 
-                            Text(spell.castingTime, Modifier.width(75.dp))
+                                Text(spell.castingTime, Modifier.width(75.dp))
 
-                            if(spell.level != 0) {
-                                Button({
-                                    Cast(spell)
-                                }) {
-                                    Text("CAST")
+                                if (spell.level != 0) {
+                                    Button({
+                                        Cast(spell)
+                                    }) {
+                                        Text("CAST")
+                                    }
                                 }
                             }
                         }
+                        Spacer(Modifier.height(2.dp))
                     }
-                    Spacer(Modifier.height(2.dp))
                 }
             }
         }

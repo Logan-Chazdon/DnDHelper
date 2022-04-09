1a07f0110b6f767353be02afac21c969b7624f6b
Logan <loganchazdon@gmail.com>
2022-04-08 20:06:15 -0600

    Optimizing BackgroundView.kt with a lazyColumn.
    Signed-off-by: Logan <loganchazdon@gmail.com>

--- app/src/main/java/com/example/dndhelper/ui/newCharacter/BackgroundView.kt
+++ app/src/main/java/com/example/dndhelper/ui/newCharacter/BackgroundView.kt
@@ -2,9 +2,9 @@ package com.example.dndhelper.ui.newCharacter
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -23,7 +23,6 @@ fun BackgroundView(
     navController: NavHostController
 ) {
     val backgrounds = viewModel.backgrounds.observeAsState()
-    val scrollState = rememberScrollState()
     viewModel.id = characterId
 
     Row(
@@ -31,55 +30,57 @@ fun BackgroundView(
            .fillMaxSize(),
        horizontalArrangement = Arrangement.Center,
     ) {
-        Column(
+        LazyColumn(
             modifier = Modifier
-                .verticalScroll(scrollState)
                 .fillMaxHeight(),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            state = rememberLazyListState()
         ) {
             backgrounds.value?.forEachIndexed { i, it ->
-                Card(
-                    backgroundColor = MaterialTheme.colors.surface,
-                    shape = RoundedCornerShape(10.dp),
-                    modifier = Modifier
-                        .fillMaxWidth(0.95f)
-                        .clickable {
-                            navController.navigate("newCharacterView/BackgroundView/ConfirmBackGroundView/$i/${viewModel.id}")
-                        },
-                    elevation = 10.dp
-                ) {
-                    Column(
-                        modifier = Modifier.padding(start = 5.dp)
+                item {
+                    Card(
+                        backgroundColor = MaterialTheme.colors.surface,
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier
+                            .fillMaxWidth(0.95f)
+                            .clickable {
+                                navController.navigate("newCharacterView/BackgroundView/ConfirmBackGroundView/$i/${viewModel.id}")
+                            },
+                        elevation = 10.dp
                     ) {
-                        Text(text = it.name, style = MaterialTheme.typography.h5)
-                        Text(
-                            text = it.desc,
-                            fontSize = 16.sp,
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 2
-                        )
-
-                        it.features.forEach { feature ->
-                            Text(text = feature.name, style = MaterialTheme.typography.h6)
+                        Column(
+                            modifier = Modifier.padding(start = 5.dp)
+                        ) {
+                            Text(text = it.name, style = MaterialTheme.typography.h5)
                             Text(
-                                text = feature.description,
-                                style = MaterialTheme.typography.subtitle2,
-                                modifier = Modifier.padding(start = 5.dp),
+                                text = it.desc,
+                                fontSize = 16.sp,
                                 overflow = TextOverflow.Ellipsis,
                                 maxLines = 2
                             )
-                        }
 
-                        if(it.languages.isNotEmpty()) {
-                            Row(modifier = Modifier.padding(start = 5.dp))
-                            {
+                            it.features.forEach { feature ->
+                                Text(text = feature.name, style = MaterialTheme.typography.h6)
                                 Text(
-                                    "Languages: "
+                                    text = feature.description,
+                                    style = MaterialTheme.typography.subtitle2,
+                                    modifier = Modifier.padding(start = 5.dp),
+                                    overflow = TextOverflow.Ellipsis,
+                                    maxLines = 2
                                 )
-                                it.languages.forEach { lang ->
+                            }
+
+                            if (it.languages.isNotEmpty()) {
+                                Row(modifier = Modifier.padding(start = 5.dp))
+                                {
                                     Text(
-                                        lang.name.toString()
+                                        "Languages: "
                                     )
+                                    it.languages.forEach { lang ->
+                                        Text(
+                                            lang.name.toString()
+                                        )
+                                    }
                                 }
                             }
                         }

a102b55166bcb6c481813c2e62ecfc21a0aa14b9
Logan <loganchazdon@gmail.com>
2022-04-25 16:29:31 -0600

    Adding a search bar to races.
    Signed-off-by: Logan <loganchazdon@gmail.com>

--- app/src/main/java/com/example/dndhelper/ui/newCharacter/RaceView.kt
+++ app/src/main/java/com/example/dndhelper/ui/newCharacter/RaceView.kt
@@ -5,13 +5,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -25,68 +26,102 @@ fun RaceView(
     viewModel.id = characterId
     val races = viewModel.races.observeAsState()
 
-
-    LazyColumn(
-        Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        state = rememberLazyListState()
-    ) {
-        races.value?.forEachIndexed { i, race ->
-            item {
-                Card(
-                    backgroundColor = MaterialTheme.colors.surface,
-                    modifier = Modifier
-                        .fillMaxWidth(0.95f)
-                        .clickable {
-                            navController.navigate("newCharacterView/ConfirmRaceView/$i/$characterId")
-                        },
-                    shape = RoundedCornerShape(10.dp),
-                    elevation = 10.dp
-                ) {
-                    Column(
-                        Modifier.padding(start = 5.dp),
-                        verticalArrangement = Arrangement.spacedBy(2.dp)
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.Bottom
+    Column {
+        var search by remember { mutableStateOf("") }
+        Row(
+            Modifier
+                .fillMaxWidth(),
+        ) {
+            TextField(
+                value = search,
+                label = {
+                    Text("Search")
+                },
+                onValueChange = {
+                    search = it
+                },
+                singleLine = true,
+                textStyle = TextStyle.Default.copy(fontSize = 20.sp),
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    Icon(
+                        Icons.Default.Search,
+                        "Search"
+                    )
+                }
+            )
+        }
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            state = rememberLazyListState()
+        ) {
+            races.value?.forEachIndexed { i, race ->
+                //TODO upgrade search
+                if (search == "" || race.name.lowercase().contains(search.lowercase())) {
+                    item {
+                        Card(
+                            backgroundColor = MaterialTheme.colors.surface,
+                            modifier = Modifier
+                                .fillMaxWidth(0.95f)
+                                .clickable {
+                                    navController.navigate("newCharacterView/ConfirmRaceView/$i/$characterId")
+                                },
+                            shape = RoundedCornerShape(10.dp),
+                            elevation = 10.dp
                         ) {
-                            Text(text = race.name, fontSize = 24.sp)
-                            Spacer(Modifier.fillMaxWidth(0.1f))
-                            Text(text = race.size, fontSize = 18.sp)
-                        }
-                        Column {
-
-                            Text(text = "Languages", style = MaterialTheme.typography.subtitle1)
-                            Row {
-                                for (language in race.languages) {
-                                    Text(
-                                        text = language.name.toString(),
-                                        modifier = Modifier.padding(start = 5.dp)
-                                    )
-                                    Spacer(modifier = Modifier.width(10.dp))
+                            Column(
+                                Modifier.padding(start = 5.dp),
+                                verticalArrangement = Arrangement.spacedBy(2.dp)
+                            ) {
+                                Row(
+                                    verticalAlignment = Alignment.Bottom
+                                ) {
+                                    Text(text = race.name, fontSize = 24.sp)
+                                    Spacer(Modifier.fillMaxWidth(0.1f))
+                                    Text(text = race.size, fontSize = 18.sp)
                                 }
-                            }
+                                Column {
 
-                            for (trait in race.traits) {
-                                Text(text = trait.name, style = MaterialTheme.typography.subtitle1)
-                                Spacer(Modifier.height(2.dp))
-                                Text(
-                                    text = trait.description,
-                                    modifier = Modifier.padding(start = 5.dp)
-                                )
-                            }
+                                    Text(
+                                        text = "Languages",
+                                        style = MaterialTheme.typography.subtitle1
+                                    )
+                                    Row {
+                                        for (language in race.languages) {
+                                            Text(
+                                                text = language.name.toString(),
+                                                modifier = Modifier.padding(start = 5.dp)
+                                            )
+                                            Spacer(modifier = Modifier.width(10.dp))
+                                        }
+                                    }
 
-                            Row()
-                            {
-                                Column() {
-                                    race.abilityBonuses?.forEach { abilityBonus ->
-                                        Text(text = abilityBonus.toString())
+                                    for (trait in race.traits) {
+                                        Text(
+                                            text = trait.name,
+                                            style = MaterialTheme.typography.subtitle1
+                                        )
+                                        Spacer(Modifier.height(2.dp))
+                                        Text(
+                                            text = trait.description,
+                                            modifier = Modifier.padding(start = 5.dp)
+                                        )
+                                    }
+
+                                    Row()
+                                    {
+                                        Column() {
+                                            race.abilityBonuses?.forEach { abilityBonus ->
+                                                Text(text = abilityBonus.toString())
+                                            }
+                                        }
+                                        Spacer(modifier = Modifier.fillMaxWidth(0.2f))
+                                        Text(text = "Speed: ${race.groundSpeed}")
                                     }
                                 }
-                                Spacer(modifier = Modifier.fillMaxWidth(0.2f))
-                                Text(text = "Speed: ${race.groundSpeed}")
                             }
                         }
                     }

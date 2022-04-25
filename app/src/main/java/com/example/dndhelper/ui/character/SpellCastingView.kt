75eac27ade840b9091315dfc1c528bc2407453c4
Logan <loganchazdon@gmail.com>
2022-04-25 15:43:11 -0600

    Adding ability to hide non prepared spells.
    Signed-off-by: Logan <loganchazdon@gmail.com>

--- app/src/main/java/com/example/dndhelper/ui/character/SpellCastingView.kt
+++ app/src/main/java/com/example/dndhelper/ui/character/SpellCastingView.kt
@@ -9,16 +9,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -30,6 +24,7 @@ import com.example.dndhelper.repository.dataClasses.Resource
 import com.example.dndhelper.repository.dataClasses.Spell
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @ExperimentalFoundationApi
 @Composable
 fun SpellCastingView(
@@ -49,6 +44,9 @@ fun SpellCastingView(
         val spellLevelsExpanded = remember {
             mutableStateListOf(true, true, true, true, true, true, true, true, true, true)
         }
+        var hideNonPreparedSpells by remember {
+            mutableStateOf(false)
+        }
         LazyColumn(
             state = state,
             modifier = Modifier.fillMaxSize(),
@@ -61,66 +59,83 @@ fun SpellCastingView(
                 item {
                     if(spells?.isNotEmpty() == true || slotLevel != 0) {
                         Row(
-                            modifier = Modifier
-                                .fillMaxWidth(0.95f)
-                                .padding(5.dp),
-                            horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.Start)
-                        ) {
-                            Row(
-                                modifier = Modifier.clickable {
-                                    spellLevelsExpanded[slotLevel] = !spellLevelsExpanded[slotLevel]
-                                }
-                            ) {
-                                val angle: Float by animateFloatAsState(
-                                    targetValue = if (spellLevelsExpanded[slotLevel]) 0F else -90F,
-                                    animationSpec = tween(
-                                        durationMillis = 150, // duration
-                                        easing = FastOutSlowInEasing
-                                    )
+                            verticalAlignment = Alignment.CenterVertically
+                        ){
+                            CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false) {
+                                Checkbox(
+                                    checked = hideNonPreparedSpells,
+                                    onCheckedChange = {
+                                        hideNonPreparedSpells = !hideNonPreparedSpells
+                                    }
                                 )
-
-                                Text(text = slots.name)
-                                Icon(
-                                    Icons.Default.ArrowDropDown, "Drop down",
-                                    Modifier.rotate(angle)
-                                )
-
                             }
-
                             Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End)
+                                modifier = Modifier
+                                    .fillMaxWidth(0.95f)
+                                    .padding(5.dp),
+                                horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.Start)
                             ) {
-                                val selected = Color.Gray
-                                val onPrimary = MaterialTheme.colors.onSurface
-                                val surface = MaterialTheme.colors.surface
-                                for (index in (0 until slots.maxAmount()).reversed()) {
-                                    Canvas(
-                                        modifier = Modifier
-                                            .size(20.dp)
-                                            .clickable {
-                                                if (slots.currentAmount > index) {
-                                                    useSlot(slotLevel)
-                                                } else {
-                                                    refundSlot(slotLevel)
-                                                }
-                                            }
-                                    ) {
-                                        drawCircle(
-                                            color = if (slots.currentAmount > index) {
-                                                surface
-                                            } else {
-                                                selected
-                                            },
-                                            center = this.center,
-                                            style = Fill
+                                Row(
+                                    modifier = Modifier.clickable {
+                                        spellLevelsExpanded[slotLevel] =
+                                            !spellLevelsExpanded[slotLevel]
+                                    }
+                                ) {
+                                    val angle: Float by animateFloatAsState(
+                                        targetValue = if (spellLevelsExpanded[slotLevel]) 0F else -90F,
+                                        animationSpec = tween(
+                                            durationMillis = 150, // duration
+                                            easing = FastOutSlowInEasing
                                         )
+                                    )
 
-                                        drawCircle(
-                                            color = onPrimary,
-                                            center = this.center,
-                                            style = Stroke(2f)
-                                        )
+                                    Text(text = slots.name)
+                                    Icon(
+                                        Icons.Default.ArrowDropDown, "Drop down",
+                                        Modifier.rotate(angle)
+                                    )
+
+                                }
+
+
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.spacedBy(
+                                        4.dp,
+                                        Alignment.End
+                                    )
+                                ) {
+                                    val selected = Color.Gray
+                                    val onPrimary = MaterialTheme.colors.onSurface
+                                    val surface = MaterialTheme.colors.surface
+                                    for (index in (0 until slots.maxAmount()).reversed()) {
+                                        Canvas(
+                                            modifier = Modifier
+                                                .size(20.dp)
+                                                .clickable {
+                                                    if (slots.currentAmount > index) {
+                                                        useSlot(slotLevel)
+                                                    } else {
+                                                        refundSlot(slotLevel)
+                                                    }
+                                                }
+                                        ) {
+                                            drawCircle(
+                                                color = if (slots.currentAmount > index) {
+                                                    surface
+                                                } else {
+                                                    selected
+                                                },
+                                                center = this.center,
+                                                style = Fill
+                                            )
+
+                                            drawCircle(
+                                                color = onPrimary,
+                                                center = this.center,
+                                                style = Stroke(2f)
+                                            )
+                                        }
                                     }
                                 }
                             }
@@ -132,60 +147,64 @@ fun SpellCastingView(
                     items(spells?.size ?: 0) { i ->
                         val spell = spells!![i].second
                         val prepared = spells[i].first
-                        Card(
-                            modifier = Modifier.fillMaxWidth(0.95f)
-                                .clickable {
-                                    cast(spell)
-                                },
-                            elevation = 2.dp
-                        ) {
-                            Row(
-                                modifier = Modifier.padding(2.dp),
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.spacedBy(2.dp)
+                        if (!hideNonPreparedSpells || prepared != false) {
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth(0.95f)
+                                    .clickable {
+                                        cast(spell)
+                                    },
+                                elevation = 2.dp
                             ) {
-                                //Preparation
-                                Box(
-                                    modifier = Modifier.width(20.dp),
+                                Row(
+                                    modifier = Modifier.padding(2.dp),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.spacedBy(2.dp)
                                 ) {
-                                    prepared?.let { isPrepared ->
-                                        if (spell.level != 0) {
-                                            val preparedColor = MaterialTheme.colors.onBackground
-                                            val onBackground = MaterialTheme.colors.onBackground
-                                            val background = MaterialTheme.colors.background
-                                            Canvas(modifier = Modifier
-                                                .size(20.dp)
-                                                .clickable {
-                                                    togglePreparation(spell)
-                                                }) {
-                                                drawCircle(
-                                                    color = if (isPrepared) {
-                                                        preparedColor
-                                                    } else {
-                                                        background
-                                                    },
-                                                    center = this.center,
-                                                    style = Fill
-                                                )
+                                    //Preparation
+                                    Box(
+                                        modifier = Modifier.width(20.dp),
+                                    ) {
+                                        prepared?.let { isPrepared ->
+                                            if (spell.level != 0) {
+                                                val preparedColor =
+                                                    MaterialTheme.colors.onBackground
+                                                val onBackground = MaterialTheme.colors.onBackground
+                                                val background = MaterialTheme.colors.background
+                                                Canvas(modifier = Modifier
+                                                    .size(20.dp)
+                                                    .clickable {
+                                                        togglePreparation(spell)
+                                                    }) {
+                                                    drawCircle(
+                                                        color = if (isPrepared) {
+                                                            preparedColor
+                                                        } else {
+                                                            background
+                                                        },
+                                                        center = this.center,
+                                                        style = Fill
+                                                    )
 
-                                                drawCircle(
-                                                    color = onBackground,
-                                                    center = this.center,
-                                                    style = Stroke(2f)
-                                                )
+                                                    drawCircle(
+                                                        color = onBackground,
+                                                        center = this.center,
+                                                        style = Stroke(2f)
+                                                    )
+                                                }
                                             }
                                         }
                                     }
+
+                                    Text(spell.name, Modifier.width(120.dp))
+
+                                    Text(spell.damage, Modifier.width(140.dp))
+
+                                    Text(spell.castingTime, Modifier.width(160.dp))
                                 }
-
-                                Text(spell.name, Modifier.width(120.dp))
-
-                                Text(spell.damage, Modifier.width(140.dp))
-
-                                Text(spell.castingTime, Modifier.width(160.dp))
                             }
+                            Spacer(Modifier.height(2.dp))
                         }
-                        Spacer(Modifier.height(2.dp))
                     }
                 }
             }

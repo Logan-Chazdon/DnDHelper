404ca1a893070d54860cdda97c31535270e7fc19
Logan <loganchazdon@gmail.com>
2022-07-04 11:10:45 -0600

    Removing unused function.
    Signed-off-by: Logan <loganchazdon@gmail.com>

--- app/src/main/java/gmail/loganchazdon/dndhelper/model/database/RoomDataBase.kt
+++ app/src/main/java/gmail/loganchazdon/dndhelper/model/database/RoomDataBase.kt
@@ -1,8 +1,6 @@
 package gmail.loganchazdon.dndhelper.model.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import gmail.loganchazdon.dndhelper.model.Character
@@ -14,23 +12,4 @@ import gmail.loganchazdon.dndhelper.model.Race
 @TypeConverters(Converters::class)
 abstract class RoomDataBase: RoomDatabase() {
     abstract fun databaseDao(): DatabaseDao
-
-    companion object  {
-        private var INSTANCE: RoomDataBase? = null
-        internal fun getDatabase(context: Context) : RoomDataBase? {
-            if(INSTANCE == null) {
-                synchronized(RoomDataBase::class.java) {
-                    INSTANCE = Room.databaseBuilder<RoomDataBase>(
-                        context.applicationContext,
-                        RoomDataBase::class.java,
-                        "database"
-                    ).fallbackToDestructiveMigration()
-                        .build()
-
-                }
-            }
-
-            return INSTANCE
-        }
-
-    }}
+}

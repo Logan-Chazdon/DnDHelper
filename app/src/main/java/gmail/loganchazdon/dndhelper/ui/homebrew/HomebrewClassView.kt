69723c53a34da157ab50e578a6f02fb1bdbbdcd4
Logan <loganchazdon@gmail.com>
2023-01-05 05:42:48 -0700

    Adding pact magic.

--- app/src/main/java/gmail/loganchazdon/dndhelper/ui/homebrew/HomebrewClassView.kt
+++ app/src/main/java/gmail/loganchazdon/dndhelper/ui/homebrew/HomebrewClassView.kt
@@ -2,18 +2,20 @@ package gmail.loganchazdon.dndhelper.ui.homebrew
 
 import android.os.Handler
 import android.os.Looper
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import gmail.loganchazdon.dndhelper.model.repositories.Repository
+import gmail.loganchazdon.dndhelper.ui.SpellDetailsView
 import gmail.loganchazdon.dndhelper.ui.newCharacter.AutoSave
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -125,6 +127,146 @@ fun HomebrewClassView(
                                 },
                                 modifier = Modifier.weight(weight = 1f, fill = true)
                             )
+                        }
+                    }
+                }
+
+                //Pact magic
+                item {
+                    AttributeView(title = "Pact Magic", active = viewModel.hasPactMagic) {
+                        //Casing mod
+                        var expanded by remember {
+                            mutableStateOf(false)
+                        }
+                        val spellsExpanded = remember {
+                            mutableStateOf(false)
+                        }
+
+                        GenericSelectionPopupView(
+                            isExpanded = spellsExpanded,
+                            onItemClick = {
+                                if(viewModel.pactMagicSpells.contains(it)) {
+                                    viewModel.pactMagicSpells.remove(it)
+                                } else {
+                                    viewModel.pactMagicSpells.add(it)
+                                }
+                            },
+                            items = viewModel.allSpells.observeAsState(emptyList()).value,
+                            detailsView = {
+                                SpellDetailsView(spell = it)
+                            },
+                            getName = {
+                                it.name
+                            },
+                            isSelected = {
+                                viewModel.pactMagicSpells.contains(it)
+                            }
+                        )
+
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable { expanded = !expanded }
+                        ) {
+                            Row {
+                                Text(
+                                    text = viewModel.pactMagicAbility.value,
+                                    modifier = Modifier.padding(5.dp).weight(1f),
+                                    style = MaterialTheme.typography.h5
+                                )
+
+                                Button(
+                                    onClick = {
+                                        spellsExpanded.value= !spellsExpanded.value
+                                    }
+                                ) {
+                                    Text("ADD SPELLS")
+                                }
+                            }
+                        }
+
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = {
+                                expanded = false
+                            }
+                        ) {
+                            Repository.statNames.forEach {
+                                DropdownMenuItem(
+                                    onClick = {
+                                        viewModel.pactMagicAbility.value = it
+                                        expanded = false
+                                    }
+                                ) {
+                                    Text(it)
+                                }
+                            }
+                        }
+
+                        Column(
+                            modifier = Modifier.height(275.dp)
+                        ) {
+                            LazyColumn {
+                                items(20) { index ->
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        horizontalArrangement = Arrangement.spacedBy(5.dp),
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                        Text(
+                                            text = (index + 1).toString(),
+                                            modifier = Modifier.weight(weight = 0.4f, fill = true),
+                                            style = MaterialTheme.typography.h6
+                                        )
+
+                                        OutlinedTextField(
+                                            value = viewModel.pactMagicSlots[index].first,
+                                            onValueChange = {
+                                                viewModel.pactMagicSlots[index] =
+                                                    Pair(it, viewModel.pactMagicSlots[index].second)
+                                            },
+                                            label = {
+                                                Text("Slot level")
+                                            },
+                                            modifier = Modifier.weight(weight = 1f, fill = true)
+                                        )
+
+                                        OutlinedTextField(
+                                            value = viewModel.pactMagicSlots[index].second,
+                                            onValueChange = {
+                                                viewModel.pactMagicSlots[index] =
+                                                    Pair(viewModel.pactMagicSlots[index].first, it)
+                                            },
+                                            label = {
+                                                Text("Amount")
+                                            },
+                                            modifier = Modifier.weight(weight = 1f, fill = true)
+                                        )
+
+                                        OutlinedTextField(
+                                            value = viewModel.pactMagicCantripsKnown[index],
+                                            onValueChange = {
+                                                viewModel.pactMagicCantripsKnown[index] = it
+                                            },
+                                            label = {
+                                                Text("Cantrips")
+                                            },
+                                            modifier = Modifier.weight(weight = 1f, fill = true)
+                                        )
+
+                                        OutlinedTextField(
+                                            value = viewModel.pactMagicSpellsKnown[index] ,
+                                            onValueChange = {
+                                                viewModel.pactMagicSpellsKnown[index]  =it
+                                            },
+                                            label = {
+                                                Text("Spells")
+                                            },
+                                            modifier = Modifier.weight(weight = 1f, fill = true)
+                                        )
+                                    }
+                                }
+                            }
                         }
                     }
                 }
--- app/src/main/java/gmail/loganchazdon/dndhelper/ui/homebrew/HomebrewClassViewModel.kt
+++ app/src/main/java/gmail/loganchazdon/dndhelper/ui/homebrew/HomebrewClassViewModel.kt
@@ -1,15 +1,14 @@
 package gmail.loganchazdon.dndhelper.ui.homebrew
 
 import android.app.Application
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import gmail.loganchazdon.dndhelper.model.Class
-import gmail.loganchazdon.dndhelper.model.ClassEntity
-import gmail.loganchazdon.dndhelper.model.Subclass
+import gmail.loganchazdon.dndhelper.model.*
 import gmail.loganchazdon.dndhelper.model.junctionEntities.ClassFeatureCrossRef
 import gmail.loganchazdon.dndhelper.model.junctionEntities.ClassSubclassCrossRef
 import gmail.loganchazdon.dndhelper.model.repositories.Repository
@@ -35,20 +34,84 @@ class HomebrewClassViewModel @Inject constructor(
     }
 
     fun saveClass() {
+        val pactSlots = mutableListOf<Resource>()
+        pactMagicSlots.forEach { (levelString, amountString) ->
+            val level = try {
+                levelString.toInt()
+            } catch (E: java.lang.Exception) {
+                1
+            }
+            val amount = try {
+                amountString.toInt()
+            } catch (E: java.lang.Exception) {
+                1
+            }
+            pactSlots.add(
+                Resource(
+                    name = Repository.allSpellLevels[level - 1].second,
+                    currentAmount = amount,
+                    maxAmountType = amount.toString(),
+                    rechargeAmountType = amount.toString()
+                )
+            )
+        }
+
+        val cantripsKnown = mutableListOf<Int>()
+        val spellsKnown = mutableListOf<Int>()
+        pactMagicCantripsKnown.forEachIndexed { index, it ->
+            cantripsKnown.add(index, try {
+                it.toInt()
+            } catch (e: java.lang.Exception) {
+                cantripsKnown.getOrNull(index - 1) ?: 0
+            })
+        }
+
+        pactMagicSpellsKnown.forEachIndexed { index, it ->
+            spellsKnown.add(index, try {
+                it.toInt()
+            } catch (e: java.lang.Exception) {
+                spellsKnown.getOrNull(index - 1) ?: 0
+            })
+        }
+
+
+        val pactMagic = PactMagic(
+            castingAbility = pactMagicAbility.value.substring(0, 3),
+            spellsKnown = spellsKnown,
+            cantripsKnown = cantripsKnown,
+            pactSlots = pactSlots
+        )
+
         repository.insertClass(
             ClassEntity(
                 name = name.value,
                 isHomebrew = true,
-                hitDie = try { hitDie.value.toInt() } catch(e: Exception) { 8 },
-                subclassLevel = try { subclassLevel.value.toInt() } catch(e: Exception) { 1 },
+                hitDie = try {
+                    hitDie.value.toInt()
+                } catch (e: Exception) {
+                    8
+                },
+                subclassLevel = try {
+                    subclassLevel.value.toInt()
+                } catch (e: Exception) {
+                    1
+                },
                 proficiencyChoices = emptyList(), //TODO
                 proficiencies = emptyList(), //TODO
                 equipmentChoices = emptyList(), //TODO
                 equipment = emptyList(), //TODO
-                startingGoldD4s =  try { goldDie.value.toInt() } catch(e: Exception) { 4 },
-                startingGoldMultiplier = try { goldMultiplier.value.toInt() } catch(e: Exception) { 10 },
+                startingGoldD4s = try {
+                    goldDie.value.toInt()
+                } catch (e: Exception) {
+                    4
+                },
+                startingGoldMultiplier = try {
+                    goldMultiplier.value.toInt()
+                } catch (e: Exception) {
+                    10
+                },
                 spellCasting = null, //TODO
-                pactMagic = null //TODO
+                pactMagic = pactMagic
             )
         )
     }
@@ -66,19 +129,97 @@ class HomebrewClassViewModel @Inject constructor(
         repository.removeClassFeatureCrossRef(
             ClassFeatureCrossRef(
                 featureId = featureId,
-                id= id
+                id = id
             )
         )
     }
 
-    val goldMultiplier= mutableStateOf("10")
+    val pactMagicSpellsKnown =
+        mutableStateListOf(
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "11",
+            "12",
+            "12",
+            "13",
+            "13",
+            "14",
+            "14",
+            "15",
+            "15",
+            "15"
+        )
+    val pactMagicCantripsKnown =
+        mutableStateListOf(
+            "2",
+            "2",
+            "2",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        )
+    val allSpells = repository.getLiveSpells()
+    val hasPactMagic = mutableStateOf(false)
+    val goldMultiplier = mutableStateOf("10")
     val goldDie = mutableStateOf("4")
     val hitDie = mutableStateOf("8")
-    var subclasses : LiveData<List<Subclass>>? = null
+    var subclasses: LiveData<List<Subclass>>? = null
     val name = mutableStateOf("")
     var clazz: LiveData<Class>? = null
     var id: Int = -1
     val subclassLevel = mutableStateOf("1")
+    val pactMagicAbility = mutableStateOf("Charisma")
+    val pactMagicSpells = mutableStateListOf<Spell>()
+
+
+    //Slot level to slot number
+    val pactMagicSlots = mutableStateListOf(
+        "1" to "1",
+        "1" to "2",
+        "2" to "2",
+        "2" to "2",
+        "3" to "2",
+        "3" to "2",
+        "4" to "2",
+        "4" to "2",
+        "5" to "2",
+        "5" to "2",
+        "5" to "3",
+        "5" to "3",
+        "5" to "3",
+        "5" to "3",
+        "5" to "3",
+        "5" to "3",
+        "5" to "3",
+        "5" to "4",
+        "5" to "4",
+        "5" to "4",
+        "5" to "4",
+    )
+
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
@@ -88,7 +229,7 @@ class HomebrewClassViewModel @Inject constructor(
                 }
                 clazz = repository.getClass(it)
                 id = it
-                subclasses  = repository.getSubclassesByClassId(id)
+                subclasses = repository.getSubclassesByClassId(id)
             }
         }
     }

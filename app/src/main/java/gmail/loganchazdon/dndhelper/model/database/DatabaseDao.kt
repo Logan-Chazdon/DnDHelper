c83a9607d4933f997c418af1bf543e94b7ca5de2
Logan <loganchazdon@gmail.com>
2023-01-11 22:12:54 -0700

    Fixing getClass functions.

--- app/src/main/java/gmail/loganchazdon/dndhelper/model/database/DatabaseDao.kt
+++ app/src/main/java/gmail/loganchazdon/dndhelper/model/database/DatabaseDao.kt
@@ -557,10 +557,15 @@ WHERE backgroundId IS :id
     fun getClass(id : Int): LiveData<Class> {
         val result = MediatorLiveData<Class>()
         result.addSource(getUnfilledClass(id)) { entity ->
-            if(entity != null) {
-                entity as Class
-                entity.levelPath = getLevelPath(id)
-                result.value = entity
+            GlobalScope.launch {
+                if (entity != null) {
+                    result.postValue(
+                        Class(
+                            entity,
+                            getLevelPath(id)
+                        )
+                    )
+                }
             }
         }
         return result

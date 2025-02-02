7edcb518b745f167a9d5903d7278733c4e5e2453
Logan <loganchazdon@gmail.com>
2025-02-02 01:51:34 -0700

    Adding subclassFeatureCrossRefTest.

--- shared/src/test/kotlin/services/SubclassServiceIntegrationTest.kt
+++ shared/src/test/kotlin/services/SubclassServiceIntegrationTest.kt
@@ -1,51 +1,73 @@
 package services
 
+import io.ktor.client.*
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
+import model.FeatureEntity
 import model.SubclassEntity
 import org.junit.Test
 
-class SubclassServiceIntegrationTest {
-    private val userOneService = SubclassService(client1)
-    private val userTwoService = SubclassService(client2)
 
+class SubclassServiceIntegrationTest {
+    private data class User(
+        val subclasses: List<SubclassData>,
+        val client: HttpClient
+    ) {
+        val subclassService = SubclassService(client)
+        val featureService = FeatureService(client)
+    }
+
+    private data class SubclassData(
+        val entity: SubclassEntity,
+        val features: List<FeatureEntity> = emptyList()
+    )
 
     private val users = listOf(
-        userOneService to listOf(
-            SubclassEntity(
-                name = "UserOne Homebrew",
-                spellAreFree = false,
-                spellCasting = null,
-                isHomebrew = true,
-            ).apply {
-                subclassId = 100
-            },
-            SubclassEntity(
-                name = "UserOne Homebrew",
-                spellAreFree = false,
-                spellCasting = null,
-                isHomebrew = true,
-            ).apply {
-                subclassId = 101
-            }
+        User(
+            client = client1,
+            subclasses = listOf(
+                SubclassData(
+                    SubclassEntity(
+                        name = "UserOne Homebrew",
+                        spellAreFree = false,
+                        spellCasting = null,
+                        isHomebrew = true,
+                    ).apply {
+                        subclassId = 100
+                    }),
+                SubclassData(
+                    SubclassEntity(
+                        name = "UserOne Homebrew",
+                        spellAreFree = false,
+                        spellCasting = null,
+                        isHomebrew = true,
+                    ).apply {
+                        subclassId = 101
+                    })
+            ),
         ),
-        userTwoService to listOf(
-            SubclassEntity(
-                name = "UserTwo Homebrew",
-                spellAreFree = false,
-                spellCasting = null,
-                isHomebrew = true,
-            ).apply {
-                subclassId = 100
-            },
-            SubclassEntity(
-                name = "UserTwo Homebrew",
-                spellAreFree = false,
-                spellCasting = null,
-                isHomebrew = true,
-            ).apply {
-                subclassId = 101
-            }
+        User(
+            client = client2,
+            subclasses = listOf(
+                SubclassData(
+                    SubclassEntity(
+                        name = "UserTwo Homebrew",
+                        spellAreFree = false,
+                        spellCasting = null,
+                        isHomebrew = true,
+                    ).apply {
+                        subclassId = 100
+                    }),
+                SubclassData(
+                    SubclassEntity(
+                        name = "UserTwo Homebrew",
+                        spellAreFree = false,
+                        spellCasting = null,
+                        isHomebrew = true,
+                    ).apply {
+                        subclassId = 101
+                    })
+            ),
         ),
     )
 
@@ -55,56 +77,98 @@ class SubclassServiceIntegrationTest {
 
     @Test
     fun insertAndGetSubclass() = runTest {
-        users.forEach {user ->
-            user.second.forEach { subclass ->
-                user.first.insertSubclass(subclass)
-                val serverSubclass = user.first.getSubclass(subclass.subclassId).first()
-                assert(subclass.name == serverSubclass.name)
+        users.forEach { user ->
+            user.subclasses.forEach { subclass ->
+                user.subclassService.insertSubclass(subclass.entity)
+                val serverSubclass = user.subclassService.getSubclass(subclass.entity.subclassId).first()
+                assert(subclass.entity.name == serverSubclass.name)
             }
         }
     }
 
 
     @Test
-    fun removeSubclassFeatureCrossRef() {
-    }
+    fun subclassFeatureCrossRefTest() = runTest {
+        users.forEach { user ->
+            user.subclasses.forEach { subclass ->
+                //Ensure the subclass is in the db.
+                user.subclassService.insertSubclass(subclass.entity)
 
-    @Test
-    fun insertSubclassFeatureCrossRef() {
+                //Insert all the features and connect them to the subclass.
+                subclass.features.forEach { feature ->
+                    user.featureService.insertFeature(feature)
+
+                    user.subclassService.insertSubclassFeatureCrossRef(
+                        subclassId = subclass.entity.subclassId,
+                        featureId = feature.featureId
+                    )
+                }
+
+                //Check that the features are returned.
+                val serverFeatures = user.subclassService.getSubclassFeatures(
+                    subclass.entity.subclassId,
+                    maxLevel = 20
+                )
+
+                assert(serverFeatures.map { it.featureId } == subclass.features.map { it.featureId })
+
+                val liveServerFeatures = user.subclassService.getSubclassLiveFeaturesById(subclass.entity.subclassId)
+
+                assert(liveServerFeatures.first().map { it.featureId } == subclass.features.map { it.featureId })
+
+
+                subclass.features.forEach { feature ->
+                    //Delete each connection.
+                    user.subclassService.removeSubclassFeatureCrossRef(
+                        subclassId = subclass.entity.subclassId,
+                        featureId = feature.featureId
+                    )
+
+                    //Ensure the features are disconnected.
+                    assert(
+                        liveServerFeatures.first().firstOrNull
+                            { it.featureId == feature.featureId } == null
+                    )
+
+                    val refreshedServerFeatures = user.subclassService.getSubclassFeatures(
+                        subclass.entity.subclassId,
+                        maxLevel = 20
+                    )
+
+
+                    assert(
+                        refreshedServerFeatures.firstOrNull
+                        { it.featureId == feature.featureId } == null
+                    )
+                }
+            }
+        }
     }
 
     @Test
     fun getHomebrewSubclasses() = runTest {
-        users.forEach {user ->
-            user.second.forEach { subclass ->
-                user.first.insertSubclass(subclass)
+        users.forEach { user ->
+            user.subclasses.forEach { subclass ->
+                user.subclassService.insertSubclass(subclass.entity)
             }
 
-            val subclasses = user.first.getHomebrewSubclasses().first()
+            val subclasses = user.subclassService.getHomebrewSubclasses().first()
             assert(subclasses.size == 2)
         }
     }
 
     @Test
-    fun getSubclassFeatures() {
-    }
-
-    @Test
-    fun getSubclassLiveFeaturesById() {
-    }
-
-    @Test
     fun deleteSubclass() = runTest {
-        users.forEach {user ->
-            user.second.forEach { subclass ->
-                user.first.insertSubclass(subclass)
+        users.forEach { user ->
+            user.subclasses.forEach { subclass ->
+                user.subclassService.insertSubclass(subclass.entity)
             }
 
-            user.second.forEach { subclass ->
-                user.first.deleteSubclass(subclass.subclassId)
+            user.subclasses.forEach { subclass ->
+                user.subclassService.deleteSubclass(subclass.entity.subclassId)
             }
 
-            val subclasses = user.first.getHomebrewSubclasses().first()
+            val subclasses = user.subclassService.getHomebrewSubclasses().first()
             assert(subclasses.isEmpty())
         }
     }

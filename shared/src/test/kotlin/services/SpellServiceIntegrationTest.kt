d19914c72c7df4d8ee5571369994b2c439ca6841
Logan <loganchazdon@gmail.com>
2025-02-03 03:13:49 -0700

    Implementing testClassCrossRefs test.

--- shared/src/test/kotlin/services/SpellServiceIntegrationTest.kt
+++ shared/src/test/kotlin/services/SpellServiceIntegrationTest.kt
@@ -3,6 +3,7 @@ package services
 import io.ktor.client.*
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
+import model.ClassEntity
 import model.Spell
 import org.junit.Test
 
@@ -16,93 +17,117 @@ class SpellServiceIntegrationTest {
 
     private data class SpellData(
         val entity: Spell,
+        val classes: List<ClassEntity> = emptyList()
     )
 
     private val users = listOf(
         User(
             client = client1,
-            spells = listOf(SpellData(
-                Spell(
-                    name = "UserOneSpellOne",
-                    level = 2,
-                    components = emptyList(),
-                    itemComponents = emptyList(),
-                    school = "",
-                    desc = "",
-                    range = "",
-                    area = "",
-                    castingTime = "",
-                    duration = "",
-                    classes = emptyList(),
-                    damage = "",
-                    isRitual = false,
-                    isHomebrew = true
-                ).apply {
-                    id = 200
-                }),
-            SpellData(
-                Spell(
-                    name = "UserOneSpellTwo",
-                    level = 2,
-                    components = emptyList(),
-                    itemComponents = emptyList(),
-                    school = "",
-                    desc = "",
-                    range = "",
-                    area = "",
-                    castingTime = "",
-                    duration = "",
-                    classes = emptyList(),
-                    damage = "",
-                    isRitual = false,
-                    isHomebrew = true
-                ).apply {
-                    id = 201
-                })
-        )),
+            spells = listOf(
+                SpellData(
+                    Spell(
+                        name = "UserOneSpellOne",
+                        level = 2,
+                        components = emptyList(),
+                        itemComponents = emptyList(),
+                        school = "",
+                        desc = "",
+                        range = "",
+                        area = "",
+                        castingTime = "",
+                        duration = "",
+                        classes = emptyList(),
+                        damage = "",
+                        isRitual = false,
+                        isHomebrew = true
+                    ).apply {
+                        id = 200
+                    },
+                    listOf(
+                        ClassEntity(
+                            id = 1000
+                        ),
+                        ClassEntity(id = 2000)
+                    )
+                ),
+                SpellData(
+                    Spell(
+                        name = "UserOneSpellTwo",
+                        level = 2,
+                        components = emptyList(),
+                        itemComponents = emptyList(),
+                        school = "",
+                        desc = "",
+                        range = "",
+                        area = "",
+                        castingTime = "",
+                        duration = "",
+                        classes = emptyList(),
+                        damage = "",
+                        isRitual = false,
+                        isHomebrew = true
+                    ).apply {
+                        id = 201
+                    },
+                    listOf(
+                        ClassEntity(
+                            id = 1001
+                        ),
+                        ClassEntity(id = 2000)
+                    )
+                )
+            )
+        ),
         User(
             client = client2,
             spells = listOf(
-            SpellData(
-                Spell(
-                    name = "UserTwoSpellOne",
-                    level = 2,
-                    components = emptyList(),
-                    itemComponents = emptyList(),
-                    school = "",
-                    desc = "",
-                    range = "",
-                    area = "",
-                    castingTime = "",
-                    duration = "",
-                    classes = emptyList(),
-                    damage = "",
-                    isRitual = false,
-                    isHomebrew = true
-                ).apply {
-                    id = 200
-                }),
-            SpellData(
-                Spell(
-                    name = "UserTwoSpellTwo",
-                    level = 2,
-                    components = emptyList(),
-                    itemComponents = emptyList(),
-                    school = "",
-                    desc = "",
-                    range = "",
-                    area = "",
-                    castingTime = "",
-                    duration = "",
-                    classes = emptyList(),
-                    damage = "",
-                    isRitual = false,
-                    isHomebrew = true
-                ).apply {
-                    id = 201
-                }
-            ))
-    ))
+                SpellData(
+                    Spell(
+                        name = "UserTwoSpellOne",
+                        level = 2,
+                        components = emptyList(),
+                        itemComponents = emptyList(),
+                        school = "",
+                        desc = "",
+                        range = "",
+                        area = "",
+                        castingTime = "",
+                        duration = "",
+                        classes = emptyList(),
+                        damage = "",
+                        isRitual = false,
+                        isHomebrew = true
+                    ).apply {
+                        id = 200
+                    },
+                    listOf(
+                        ClassEntity(
+                            id = 1001
+                        ),
+                        ClassEntity(id = 2000)
+                    )
+                ),
+                SpellData(
+                    Spell(
+                        name = "UserTwoSpellTwo",
+                        level = 2,
+                        components = emptyList(),
+                        itemComponents = emptyList(),
+                        school = "",
+                        desc = "",
+                        range = "",
+                        area = "",
+                        castingTime = "",
+                        duration = "",
+                        classes = emptyList(),
+                        damage = "",
+                        isRitual = false,
+                        isHomebrew = true
+                    ).apply {
+                        id = 201
+                    }
+                )
+            )))
 
 
     @Test
@@ -121,9 +146,35 @@ class SpellServiceIntegrationTest {
 
 
     @Test
-    fun addAndRemoveClassSpellCrossRef() {
+    fun testClassCrossRefs() = runTest {
         users.forEach { user ->
             user.spells.forEach {
+                user.spellService.insertSpell(it.entity)
+
+                it.classes.forEach { clazz ->
+                    user.classService.insertClass(clazz)
+
+                    user.spellService.addClassSpellCrossRef(
+                        classId = clazz.id,
+                        spellId = it.entity.id
+                    )
+                }
+
+                val classes = user.spellService.getSpellClasses(it.entity.id)
+
+                assert(it.classes.map { it.id} == classes.first().map { it.id} )
+
+
+                it.classes.forEach { clazz ->
+                    user.classService.insertClass(clazz)
+
+                    user.spellService.removeClassSpellCrossRef(
+                        classId = clazz.id,
+                        spellId = it.entity.id
+                    )
+                }
+
+                assert(classes.first().isEmpty())
             }
         }
     }
@@ -158,9 +209,6 @@ class SpellServiceIntegrationTest {
         }
     }
 
-    @Test
-    fun getSpellClasses() {
-    }
 
     @Test
     fun removeSpellById() = runTest {

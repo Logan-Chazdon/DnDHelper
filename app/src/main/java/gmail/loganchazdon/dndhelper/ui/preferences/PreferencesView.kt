d38ca3e0b4a56a34db24e56f807856eafa4d90f2
Logan <loganchazdon@gmail.com>
2026-04-16 19:41:45 -0600

    Updating default preferences and adding content padding.

--- app/src/main/java/gmail/loganchazdon/dndhelper/ui/preferences/PreferencesView.kt
+++ app/src/main/java/gmail/loganchazdon/dndhelper/ui/preferences/PreferencesView.kt
@@ -1,8 +1,14 @@
 package gmail.loganchazdon.dndhelper.ui.preferences
 
 import android.annotation.SuppressLint
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.jamal.composeprefs.ui.PrefsScreen
 import com.jamal.composeprefs.ui.prefs.SwitchPref
@@ -14,10 +20,10 @@ import gmail.loganchazdon.dndhelper.dataStore
 @Composable
 fun PreferencesView() {
     Scaffold(topBar = { SettingsTopBar() }) {
-        PrefsScreen(dataStore = LocalContext.current.dataStore) {
+        PrefsScreen(dataStore = LocalContext.current.dataStore, Modifier.padding(it)) {
             prefsGroup("Theme") {
                 prefsItem { SwitchPref(key = "dark_mode", title = "Dark mode") }
-                prefsItem { SwitchPref(key = "grid_not_row", title = "Full size personality details") }
+                prefsItem { SwitchPref(key = "grid_not_row", title = "Full size personality details", defaultChecked = false) }
             }
             prefsGroup("Character Creation") {
                 prefsItem { SwitchPref(key = "auto_save", title = "Auto Save", defaultChecked = true) }
